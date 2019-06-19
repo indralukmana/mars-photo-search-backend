b5ea1ad730ea466e7a8ae17e40c8bcc62c8bd26c
Indra Lukmana <indralukmana@live.com>
2019-06-19 11:45:39 +0700

    Clean up commented codes

--- index.js
+++ index.js
@@ -3,24 +3,19 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
-//ecample: https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY
+//example: https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY
 const URL = `https://api.nasa.gov/mars-photos/api/v1/rovers`;
 const DEFAULT_SOL = 1;
 const DEFAULT_CAMERA = 'mast';
 const API_KEY = 'DEMO_KEY';
 
-// app.get('/', (req, res) => res.send('Hello World!'));
-// app.get('/', (req, res) => res.json({ content: 'Hssslo World!' }));
-
 app.get('/api/search/sol/:sol/camera/:camera', (req, res) => {
-	// const result = axios.get(`${url}/curiosity/photos?sol=1$api_key=DEMO_KEY`);
 
 	const sol = req.params.sol;
 	const camera = req.params.camera;
 
 	axios
 		.get(
-			// 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY'
 			`${URL}/curiosity/photos?sol=${sol ? sol : DEFAULT_SOL}&camera=${
 				camera ? camera : DEFAULT_CAMERA
 			}&api_key=${API_KEY}`
