4d730d3d3284e4c07b1d0a9c072517820a68f2e4
Indra Lukmana <indralukmana@live.com>
2019-06-19 14:15:47 +0700

    Add basic fetch on the API and display

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<h1>Mars Rovers Photo Search App</h1>
-				<p>Under furious development</p>
-			</header>
-		</div>
-	);
+const API = 'http://localhost:3333/api/search/sol/1/camera/mast';
+
+class App extends Component {
+	state = {
+		data: [],
+		error: null,
+		loading: false
+	};
+
+	// TODO: Handle error
+	componentDidMount() {
+		fetch(API)
+			.then(response => response.json())
+			.then(result => {
+				this.setState({ data: result.data });
+			})
+			.catch(error => this.setState({ error }));
+	}
+
+	render() {
+		const { data } = this.state;
+
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1>Mars Rovers Photo Search App</h1>
+					<p>Under furious development</p>
+					<p>{API}</p>
+					{data.map(item => (
+						<img key={item.id} src={item.img_src} alt="mars" />
+					))}
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
