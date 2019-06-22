2100afae23a8cdafd7f62436de5739da66892c0e
Indra Lukmana <indralukmana@live.com>
2019-06-22 20:12:31 +0700

    Clean up

--- client/src/Components/Search.js
+++ client/src/Components/Search.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
